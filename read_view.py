7e25d76e3278764b733dc9b4baffefda2917a287
mansi <mansis@triazinesoft.com>
2026-04-21 14:53:16 +0530

    updated plant as user input

--- read_view.py
+++ read_view.py
@@ -6,13 +6,21 @@ from dotenv import load_dotenv
 # Load environment variables from .env file
 load_dotenv()
 
+# # DB credentials (can come from environment variables)
+# DB_HOST = os.getenv("DB_HOST", "localhost")
+# DB_USER = os.getenv("DB_USER", "root")
+# DB_PASSWORD = os.getenv("DB_PASSWORD", "Triazine#123!")
+# DB_NAME = os.getenv("DB_NAME", "wps_erp")
+# DB_PORT = int(os.getenv("DB_PORT", 3306))
+# DB_TABLE = os.getenv("DB_TABLE", "incident_prediction_data_new2")  # fallback table
+
 # DB credentials (can come from environment variables)
-DB_HOST = os.getenv("DB_HOST", "localhost")
-DB_USER = os.getenv("DB_USER", "root")
-DB_PASSWORD = os.getenv("DB_PASSWORD", "Triazine#123!")
-DB_NAME = os.getenv("DB_NAME", "wps_erp")
+DB_HOST = os.getenv("DB_HOST", "49.50.69.230")
+DB_USER = os.getenv("DB_USER", "phs_demo")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "K1P5m3sCv!%")
+DB_NAME = os.getenv("DB_NAME", "pehs_ai")
 DB_PORT = int(os.getenv("DB_PORT", 3306))
-DB_TABLE = os.getenv("DB_TABLE", "incident_prediction_data_new2")  # fallback table
+DB_TABLE = os.getenv("DB_TABLE", "incident_prediction_data")  # fallback table
 
 # ------------------------
 # SQLAlchemy connection
